15f197154ee2b49623e18940abef49f13120b26a
nicolaGuerrieri <guerrierinicola@gmail.com>
2017-05-03 15:18:22 +0200

    header problem

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ var configEnv= require('../../conf-env.js');
 log.info('urldb', configEnv.ambiente.urldb,'', '');
 
 var db;
-var channel = "/dev";
+var channel = "/devtest";
 router.use(passport.initialize());
 router.use(passport.session());
 
@@ -244,6 +244,7 @@ router
 				});
 
 router.get('/getListaForCity', function(req, res) {
+	
 	var city = "";
 	if (req.query.citta) {
 		// dobbiamo cercare du db
@@ -252,7 +253,7 @@ router.get('/getListaForCity', function(req, res) {
 	} else {
 		log.info('/getLuogoById', channel, 'city: ', 'non presente');
 	}
-
+	console.log("citta " + city);
 	res.writeHead(200, {
 		"Content-Type" : "application/json",
 		"Access-Control-Allow-Origin": "*"
@@ -277,7 +278,7 @@ router.get('/getListaForCity', function(req, res) {
 				var json = JSON.stringify({
 					listaLuoghi : lista
 				});
-					res.setHeader("Access-Control-Allow-Origin", "*");
+				//	res.setHeader("Access-Control-Allow-Origin", "*");
 
 				res.end(json);
 			}
@@ -286,6 +287,19 @@ router.get('/getListaForCity', function(req, res) {
 	});
 });
 
+router.get('/attivita', function(req, res) {
+	res.writeHead(200, {
+		"Content-Type" : "application/json"
+	});
+	var lista = [{'nome': 'cali', 'img': 'cali.png', 'selezionato': false}, {'nome': 'vita', 'img': 'vita.png', 'selezionato': false},{'nome': 'trek', 'img': 'trek.png', 'selezionato': false}, {'nome': 'pesi', 'img': 'pesi.png', 'selezionato': false}, {'nome': 'anelli', 'img': 'anelli.png', 'selezionato': false},{'nome': 'walk', 'selezionato': false}, {'nome': 'american-football', 'selezionato': false}, {'nome': 'basketball', 'selezionato': false}, {'nome': 'bicycle', 'selezionato': false}, {'nome': 'body', 'selezionato': false}, {'nome': 'football', 'selezionato': false}, {'nome': 'tennisball', 'selezionato': false}, {'nome': 'water', 'selezionato': false}, {'nome': 'paw', 'selezionato': false}, {'nome': 'pizza', 'selezionato': false}, {'nome': 'restaurant', 'selezionato': false}, {'nome': 'cafe', 'selezionato': false}];
+ 
+	var attivita = JSON.stringify({
+		listaAttivita : lista
+	});
+	
+	res.end(attivita);
+ 
+});
 
 router.all('/verify', function(req, res) {
 		log.info('/verify', channel, '', '');
@@ -584,18 +598,4 @@ router.get('/getOrganizzazioni', function(req, res) {
 	});
 });
 
-
-router.get('/attivita', function(req, res) {
-	res.writeHead(200, {
-		"Content-Type" : "application/json"
-	});
-	var lista = [{'nome': 'cali', 'img': 'cali.png', 'selezionato': false}, {'nome': 'vita', 'img': 'vita.png', 'selezionato': false},{'nome': 'trek', 'img': 'trek.png', 'selezionato': false}, {'nome': 'pesi', 'img': 'pesi.png', 'selezionato': false}, {'nome': 'anelli', 'img': 'anelli.png', 'selezionato': false},{'nome': 'walk', 'selezionato': false}, {'nome': 'american-football', 'selezionato': false}, {'nome': 'basketball', 'selezionato': false}, {'nome': 'bicycle', 'selezionato': false}, {'nome': 'body', 'selezionato': false}, {'nome': 'football', 'selezionato': false}, {'nome': 'tennisball', 'selezionato': false}, {'nome': 'water', 'selezionato': false}, {'nome': 'paw', 'selezionato': false}, {'nome': 'pizza', 'selezionato': false}, {'nome': 'restaurant', 'selezionato': false}, {'nome': 'cafe', 'selezionato': false}];
- 
-	var attivita = JSON.stringify({
-		listaAttivita : lista
-	});
-	
-	res.end(attivita);
- 
-});
 module.exports = router;
